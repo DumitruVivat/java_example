28dd18dba2edbf5657f1abb240dc0b2a8036771b
Dumitru Vivat <kaibaman94@gmail.com>
2024-11-01 14:18:33 +0200

    first commit

---
+++ src/main/lesson1/Lesson1.java
@@ -0,0 +1,30 @@
+package main.lesson1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+//        int - 32 4, int / 0
+//        long - 64 8,
+
+//        float - 32 4, /0 - Float,Nan
+//        double - 64 8,
+
+//        short - 16,
+//        boolean - 1 (8),
+//        byte - 8,
+//        char
+
+        String str = "Hello World";
+        String chars = new String(new char[]{'H', 'e', 'l','l','o'});
+
+        System.out.println(str);
+        System.out.println(chars);
+
+        long l = 123;
+
+        System.out.println(1 + l);
+        // 4 - 1 2 3 4 = 10
+
+
+
+    }
+}
